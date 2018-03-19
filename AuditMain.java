cb19ffbe15973e48421602dbde36e6b8f5323bfa
Neikho <albathomas62250@gmail.com>
2018-03-19 16:39:40 +0100

    Extraire données du fichier targetDB OK.

--- AuditMain.java
+++ AuditMain.java
@@ -1,6 +1,6 @@
 //PACKAGES.
 import java.io.*;
-import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 
 public class AuditMain
@@ -8,16 +8,17 @@ public class AuditMain
     public static void main(String[] args)
     {
       //Var for reading the file that contains the database conf (IP, port, user, pas ...).
-      String v_dbConfFilePath         = new String("targetDB.conf");
-        File v_targetDBFile           = new File(v_dbConfFilePath);
-      BufferedReader v_buff           = null;
-        int v_isReading               = 1;
-      Map<String, String> v_mapDBConf = new HashMap<>();
-        String v_paramMap1            = "";
-        String v_paramMap2            = "";
-        String v_concat               = "";
+      String v_dbConfFilePath             = new String("targetDB.conf");
+        File v_targetDBFile               = new File(v_dbConfFilePath);
+      BufferedReader v_buff               = null;
+        int v_isReading                   = 1;
+      TreeMap<String, String> v_mapDBConf = new TreeMap<>();
+        String v_paramMap1                = "";
+        String v_paramMap2                = "";
+        String v_concat                   = "";
 
       //Browses and extract data from DB target conf file.
+      //List of params in this file : DB_IP_ADDR, DB_PORT, DB_TYPE, DB_NAME, DB_USER, DB_PASS
       try
       {
         v_buff = new BufferedReader(new FileReader(v_targetDBFile));
@@ -31,19 +32,19 @@ public class AuditMain
       {
         while ((v_isReading = v_buff.read()) != -1)
         {
-          System.out.println((char) v_isReading);
             switch ((char) v_isReading)
             {
-              case ';':
-                v_paramMap2 = v_concat;
-                v_concat= "";
-                break;
               case '=':
                 v_paramMap1 = v_concat;
                 v_concat= "";
                 break;
+              case ';':
+                v_paramMap2 = v_concat;
+                v_concat= "";
+                break;
               case '\n':
-                System.out.println(v_paramMap1 + "//" + v_paramMap2);
+                v_mapDBConf.put(v_paramMap1, v_paramMap2);
+                v_concat = "";
                 break;
               default:
                 v_concat= v_concat + (char) v_isReading;
@@ -66,5 +67,7 @@ public class AuditMain
           e.printStackTrace();
         }
       }
+      System.out.println(v_mapDBConf);
+      System.out.println(v_mapDBConf.get("DB_IP_ADDR"));
     }
 }
